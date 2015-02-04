f2c4d91aa50524e248a08c1ca9e991c92712fa71
bbb324 <xiejun325@gmail.com>
2015-02-04 18:53:34 -0500

    draw_board_1
    implement html5 canvas function

---
+++ colors.js
@@ -0,0 +1,20 @@
+/**
+ * Created by junxie on 12/22/14.
+ */
+
+var swatches = document.getElementsByClassName('swatch');
+for(var i= 0, n=swatches.length; i<n; i++)
+{
+    swatches[i].addEventListener('click', setSwatch);
+}
+
+function setColor(color){
+
+}
+
+function setSwatch(e)
+{
+    var swatch = e.target;
+
+
+}
