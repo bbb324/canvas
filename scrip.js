727086a69dd61bd5f345f748d7de6ead39fd5de4
bbb324 <xiejun325@gmail.com>
2017-06-13 14:22:33 +0800

    改成es

--- scrip.js
+++ scrip.js
@@ -2,54 +2,70 @@
  * Created by junxie on 12/22/14.
  */
 
+class abc {
+  constructor(x, y) {
+    this.canvas = document.getElementById("canvas");
+    this.context = canvas.getContext('2d');
+    this.colors = document.getElementById('colors');
+    this.radius = 10;
+    this.dragging = false;
+    this.color = 'black';
 
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext('2d');
-
-var radius=10;
-
-var dragging=false;
-
-canvas.width = window.screen.width;
-canvas.height = window.screen.height;
-
-context.lineWidth=radius*2;
-
-var putPoint = function(e)
-{
-  let x, y;
-  if(e.touches!=undefined) {
-    x = e.touches[0].clientX;
-    y = e.touches[0].clientY;
-  } else {
-    x = e.clientX;
-    y = e.clientY;
   }
-    if(dragging) {
-        context.lineTo(x, y);
-        context.stroke();
-        context.beginPath();
-        context.arc(x, y, radius, 0, Math.PI * 2);
-        context.fill();
-        context.beginPath();
-        context.moveTo(x, y);
+  init() {
+    let self = this;
+    this.canvas.width = window.screen.width;
+    this.canvas.height = window.screen.height;
+    this.context.lineWidth = this.adius * 2;
+    this.canvas.addEventListener('touchstart', function(e) {
+      self.engage(e);
+    });
+    this.canvas.addEventListener('touchmove', function(e) {
+      self.putPoint(e);
+    });
+    this.canvas.addEventListener('touchend', function(e) {
+      self.disengage(e)
+    });
+    this.colors.addEventListener('click', function(e) {
+      self.chooseColor(e);
+    })
+  }
+  engage(e) {
+    this.dragging = true;
+    this.context.beginPath();
+    this.putPoint(e.touches[0]);
+  }
+  disengage() {
+    this.dragging = false;
+    this.context.beginPath();
+  }
+  putPoint(e) {
+    let x, y;
+    if(e.touches != undefined) {
+      x = e.touches[0].clientX;
+      y = e.touches[0].clientY;
     }
+    else {
+      x = e.clientX;
+      y = e.clientY;
+    }
+    if(this.dragging) {
+      this.context.strokeStyle = this.color;
+      this.context.lineTo(x, y);
+      this.context.stroke();
+      this.context.beginPath();
+      this.context.arc(x, y, this.radius, 0, Math.PI * 2);
+      this.context.fill();
+      this.context.fillStyle = this.color;
+      this.context.beginPath();
+      this.context.moveTo(x, y);
+    }
+  }
+  chooseColor(e) {
+    this.color = e.target.dataset.color;
+  }
+
 }
-
-var engage = function(e)
-{
-    dragging = true;
-    putPoint(e.touches[0]);
-}
-
-var disengage = function()
-{
-    dragging = false;
-    context.beginPath();
-}
-
-
-canvas.addEventListener('touchstart', engage);
-canvas.addEventListener('touchmove', putPoint);
-canvas.addEventListener('touchend', disengage);
+let _instance = new abc();
+_instance.init();
 
