c5e45f3bd268f1edae31818e5e4fc8cabc4b30f5
bbb324 <xiejun325@gmail.com>
2017-06-13 11:52:52 +0800

    update ignore

--- scrip.js
+++ scrip.js
@@ -10,29 +10,36 @@ var radius=10;
 
 var dragging=false;
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = window.screen.width;
+canvas.height = window.screen.height;
 
 context.lineWidth=radius*2;
 
 var putPoint = function(e)
 {
+  let x, y;
+  if(e.touches!=undefined) {
+    x = e.touches[0].clientX;
+    y = e.touches[0].clientY;
+  } else {
+    x = e.clientX;
+    y = e.clientY;
+  }
     if(dragging) {
-        context.lineTo(e.clientX, e.clientY);
+        context.lineTo(x, y);
         context.stroke();
         context.beginPath();
-        context.arc(e.clientX, e.clientY, radius, 0, Math.PI * 2);
+        context.arc(x, y, radius, 0, Math.PI * 2);
         context.fill();
-
         context.beginPath();
-        context.moveTo(e.clientX, e.clientY);
+        context.moveTo(x, y);
     }
 }
 
 var engage = function(e)
 {
     dragging = true;
-    putPoint(e);
+    putPoint(e.touches[0]);
 }
 
 var disengage = function()
@@ -42,7 +49,7 @@ var disengage = function()
 }
 
 
-canvas.addEventListener('mousedown', engage);
-canvas.addEventListener('mousemove', putPoint);
-canvas.addEventListener('mouseup', disengage);
+canvas.addEventListener('touchstart', engage);
+canvas.addEventListener('touchmove', putPoint);
+canvas.addEventListener('touchend', disengage);
 
